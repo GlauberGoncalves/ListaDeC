bb44091fb91b8286117eef914d5e175ea0978883
Glauber <glauber_g3@yahoo.com.br>
2016-04-24 04:56:38 +0000

    vs2

--- ex8.c
+++ ex8.c
@@ -14,29 +14,55 @@ int main()
 {
     //declaração das variaveis 
     
-    int candidato1, candidato2, voto;
-    char decisao[2];
-  
+    int candidato1, candidato2, voto, decisao;
+    float porc1,porc2;
+    
+    candidato1=0;
+    candidato2=0;
     //requisitando dados do usuario
- do
-    {
+  do
+  {
+      system("clear");
         printf("\nInforme o numero de seu candidato (1 ou 2)\n ");
         scanf("%d",&voto); 
+       
         // Area de processamento dos dados 
         
             if (voto == 1)
-                candidato1=candidato1+1;
-            else candidato2=candidato2+1;    
-        
-        
-        printf("\n Deseja continuar (s/n)\n");
-        scanf("%c", decisao);
-    }
- while (decisao != "n");
+                candidato1 = candidato1 + 1;
+            else if (voto == 2)
+                candidato2 = candidato2 + 1;
+            system("cls || clear");  
+                voto = 0;
 
-    
-    // Resposta ao usuario
-
+        // Pergunta ao usuario se havera outro voto
+        
+        printf("\n Deseja continuar\n");
+        printf("\n 1 - Sim\n");
+        printf("\n 2 - Não\n");
+        scanf("%d",&decisao);
+        system("clear");
+  }  
+  while (decisao != 2);
+    // Porcentagem dos votos
+    voto = candidato1 + candidato2;
+    porc1 = (((float)candidato1 / voto)*100);
+    porc2 = (((float)candidato2 / voto)*100);
+  
+     // Mostra os votos
+     system("cls || clear");
+         printf("\n*************************************");
+         printf("\n***  Fim da votação               ");
+         printf("\n***                               ");
+         printf("\n*** Candidato 1..: %d porcent     ",(int)porc1);
+         printf("\n*** Candidato 2..: %d porcent     ",(int)porc2);
+         printf("\n***                               ");
+         printf("\n**********************************\n");
+         printf("\n**********************************");        
+         printf("\n***      Total de votos           ");
+         printf("\n***  candidato 1: %d              ", candidato1);
+         printf("\n***  candidato 2: %d              ", candidato2);
+         printf("\n**********************************\n");
     return 0;
     
     
